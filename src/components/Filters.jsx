ca67d2d7434cef1b8b2259b40308b17f89af845e
Lejla <lejladzanko@gmail.com>
2024-03-27 17:39:42 +0100

    onSubmit enter controlled

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,8 +2,11 @@ import PropTypes from 'prop-types';
 import FilterByName from './FilterByName';
 
 function Filters({ onChangeName, valueName }) {
+  const handleSubmit = event => {
+    event.preventDefault(); 
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FilterByName onChangeName={onChangeName} valueName={valueName} />
     </form>
   );
